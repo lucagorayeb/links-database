ea3d4eae582044fe0f83c6df7ff5f544aa41c6a2
Luca Gorayeb <lucagorayeb@gmail.com>
2026-07-05 15:08:33 -0400

    Creanting the config directory to saving all kinds of files related to rconfigurations. I also move the config.py to this directory. I also create a new file called init.py that resolves the importation problem

--- config/__init__.py
+++ config/__init__.py
@@ -1,21 +1,13 @@
 #!/usr/bin/env python
 """
 -----------------------------------------------------
-Program    : config.py
+Program    : __init__.py
 Description:
 Version    : 0.1
 Author     : Luca Gorayeb <lucagorayeb@gmail.com>
-Date       : 25/06/2026
+Date       : 04/07/2026
 Lincence   : GNU/GPL v3.0
 -----------------------------------------------------
 Use:
 -----------------------------------------------------
 """
-from pathlib import Path
-import subprocess
-
-def verify_database():
-    db_path = Path('links.sqlite3');
-    if not db_path.exists():
-        subprocess.run(['sqlite3', 'links.sqlite3', '<', 'schema.sql'])
-
