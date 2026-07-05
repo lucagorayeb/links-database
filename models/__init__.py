d020ca6b8f100df8e22a1b1a7d1407ce6cc1c0e7
Luca Gorayeb <lucagorayeb@gmail.com>
2026-07-05 12:20:50 -0400

    Creanting the models directory to saving all kinds of files related to repositorys. I also move the models.py to this file. I also create a new file called init.py that resolves the importation problem.

---
+++ models/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+-----------------------------------------------------
+Program    : __init__.py
+Description:
+Version    : 0.1
+Author     : Luca Gorayeb <lucagorayeb@gmail.com>
+Date       : 04/07/2026
+Lincence   : GNU/GPL v3.0
+-----------------------------------------------------
+Use:
+-----------------------------------------------------
+"""
