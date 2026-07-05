27431a54e7242f7371645c70b0edc665f1f925ed
Luca Gorayeb <lucagorayeb@gmail.com>
2026-07-05 15:14:32 -0400

    Commenting stuff in the main.py because the feature is not ready. init.py is a importation file.

--- __init__.py
+++ __init__.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python
 """
 -----------------------------------------------------
-Program    : main.py
+Program    : __init__.py
 Description:
 Version    : 0.1
 Author     : Luca Gorayeb <lucagorayeb@gmail.com>
-Date       : 24/06/2026
+Date       : 04/07/2026
 Lincence   : GNU/GPL v3.0
 -----------------------------------------------------
 Use:
 -----------------------------------------------------
 """
-from routes.cli_api import user_menu, user_interaction
-""" from config import (
-    verify_database,
-    security_backup
-)
-
-verify_database()
-security_backup() """
-user_menu()
-user_interaction()
+from .models import models
+from .services import services
+from .controllers import controllers
+from .repository import repository
+from .routes import cli_api, web
