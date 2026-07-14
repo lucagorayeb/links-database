1aacdd4f5d956bf7e8001542ae6514597db17ba1
Luca Gorayeb <lucagorayeb@gmail.com>
2026-07-14 00:47:09 -0400

    Adding the web api and the firsts views of links vault.

--- routes/application.py
+++ routes/application.py
@@ -6,9 +6,18 @@ Description:
 Version    : 0.1
 Author     : Luca Gorayeb <lucagorayeb@gmail.com>
 Date       : 24/06/2026
-Lincence   : GNU/GPL v3.0
+Licence   : GNU/GPL v3.0
 -----------------------------------------------------
 Use:
 -----------------------------------------------------
 """
-import fastapi 
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+@app.route("/links", methods=["GET"])
+def links():
+    return render_template("links.html")
